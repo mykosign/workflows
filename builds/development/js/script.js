338674da9f659adb58d01a9d022fe0da29e7689e
Mykolas <mykolas.tikuisis@gmail.com>
2015-09-04 00:23:10 +0000

    workflows grunt

---
+++ builds/development/js/script.js
@@ -0,0 +1,13 @@
+
+
+//------------------------------------$(document).ready(function($) {
+	$('h2').click(function(event) {
+		/* Act on the event */
+		$(this).css({
+			color: '#ffffff',
+			font-size: '10px'
+		});
+	});
+});
+
+getElementsByTagName('h2');
---
+++ components/scripts/move.js
@@ -0,0 +1,11 @@
+$(document).ready(function($) {
+	$('h2').click(function(event) {
+		/* Act on the event */
+		$(this).css({
+			color: '#ffffff',
+			font-size: '10px'
+		});
+	});
+});
+
+getElementsByTagName('h2');
